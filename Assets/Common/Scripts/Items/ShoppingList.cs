62017637b6a9984e0e42aed4a00d647ddf4511ae
Michal Tomášek <michal.tomasek6@seznam.cz>
2024-05-04 22:31:09 +0200

    Modified scene, added shelfs and used models. Added shopping list condition

--- Assets/Common/Scripts/Items/ShoppingList.cs
+++ Assets/Common/Scripts/Items/ShoppingList.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class ShoppingList : MonoBehaviour
 {
-    public bool HasMilk; // In the future this will change for hasAllItems or something like that
+    public bool HasAllItems = false; // In the future this will change for hasAllItems or something like that
+
+    private Dictionary<string, int> shoppingList = new Dictionary<string, int>();
+
+    private void Update()
+    {
+        if (shoppingList.ContainsKey("Milk") && shoppingList.ContainsKey("Apple") && shoppingList.ContainsKey("Bread"))
+        {
+            if (shoppingList["Milk"] == 2)
+            {
+                HasAllItems = true;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Milk")
+        if (other.gameObject.tag == "Item")
         {
-            HasMilk = true;
-            Debug.Log("mam mleko");
+            if (!shoppingList.ContainsKey(other.GetComponent<ItemScript>().itemName))
+            {
+                shoppingList.Add(other.GetComponent<ItemScript>().itemName, 1);
+            }
+            else
+            {
+                shoppingList[other.GetComponent<ItemScript>().itemName] += 1;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Milk")
+        if (other.gameObject.tag == "Item")
         {
-            HasMilk = false;
+            if (shoppingList[other.GetComponent<ItemScript>().itemName] == 1)
+            {
+                shoppingList.Remove(other.GetComponent<ItemScript>().itemName);
+            }
+            else
+            {
+                shoppingList[other.GetComponent<ItemScript>().itemName] -= 1;
+            }
         }
     }
 }
